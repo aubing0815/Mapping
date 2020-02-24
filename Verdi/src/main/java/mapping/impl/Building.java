b3f1a07ac8bd6b7b22adbfd8b1e71ae6c0105813
aubing0815 <manuel3046@googlemail.com>
2020-02-24 22:53:08 +0100

    Building vergessen einzuchecken

---
+++ Verdi/src/main/java/mapping/impl/Building.java
@@ -0,0 +1,16 @@
+package mapping.impl;
+
+public class Building 
+{
+	private final int building;
+	private final int floor;
+	private final int room;
+	
+	public Building(int building, int floor, int room)
+	{
+		this.building = building;
+		this.floor = floor;
+		this.room = room;
+	}
+	
+}
